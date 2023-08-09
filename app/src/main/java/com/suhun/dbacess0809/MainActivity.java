6d1b076fdf15b3596b1dda4a5261aa478a2bef90
suhunlin <lin.suhun@msa.hinet.net>
2023-08-09 16:51:42 +0800

    Add MyDBHelper and initial db

--- app/src/main/java/com/suhun/dbacess0809/MainActivity.java
+++ app/src/main/java/com/suhun/dbacess0809/MainActivity.java
@@ -2,20 +2,25 @@ package com.suhun.dbacess0809;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    TextView result;
-    EditText cname, ctel, cbirthday;
+    private TextView result;
+    private EditText cname, ctel, cbirthday;
+    private MyDBHelper myDBHelper;
+    private SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
+        myDBHelper = new MyDBHelper(this, "suhunDB", null, 1);
+        db = myDBHelper.getWritableDatabase();
     }
 
     private void initView(){
---
+++ app/src/main/java/com/suhun/dbacess0809/MyDBHelper.java
@@ -0,0 +1,25 @@
+package com.suhun.dbacess0809;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class MyDBHelper extends SQLiteOpenHelper {
+    private final String createTable = "CREATE TABLE cust (cid INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            "cname TEXT, ctel TEXT, cbirthday DATE)";
+    public MyDBHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(createTable);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
