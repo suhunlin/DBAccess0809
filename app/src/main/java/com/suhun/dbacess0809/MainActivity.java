ac245598d5fda9b6a66099b1842a592c20d857ff
suhunlin <lin.suhun@msa.hinet.net>
2023-08-09 16:42:00 +0800

    Add layout and initial layout

--- app/src/main/java/com/suhun/dbacess0809/MainActivity.java
+++ app/src/main/java/com/suhun/dbacess0809/MainActivity.java
@@ -3,12 +3,37 @@ package com.suhun.dbacess0809;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView result;
+    EditText cname, ctel, cbirthday;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView(){
+        result = findViewById(R.id.resultLog);
+        cname = findViewById(R.id.name);
+        ctel = findViewById(R.id.tel);
+        cbirthday = findViewById(R.id.birthday);
+    }
+
+    public void queryFun(View view){
+
+    }
+
+    public void insertFun(View view){
+
+    }
+
+    public void delFun(View view){
+
     }
 }
